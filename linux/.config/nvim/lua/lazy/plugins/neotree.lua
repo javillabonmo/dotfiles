b2a530c4230405818d096fff5ae45d351483c22e
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2026-06-06 11:48:59 -0500

    neotree conf

--- linux/.config/nvim/lua/lazy/plugins/neotree.lua
+++ linux/.config/nvim/lua/lazy/plugins/neotree.lua
@@ -2,6 +2,13 @@
 local plugin = {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
+  keys = {
+        {
+            "<leader>e",
+            "<cmd>Neotree toggle<cr>",
+            desc = "Explorer",
+        },
+    },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
