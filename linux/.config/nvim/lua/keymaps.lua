965cf64fab14a8acd154394ac455954483ba9c27
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2026-06-08 11:07:16 -0700

    error: syntax-highligter

--- linux/.config/nvim/lua/keymaps.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<leader>wm", "<C-w>h")
-vim.keymap.set("n", "<leader>wn", "<C-w>j")
-vim.keymap.set("n", "<leader>we", "<C-w>k")
-vim.keymap.set("n", "<leader>wi", "<C-w>l")
--- linux/.config/nvim/lua/lazy/plugins/color-highligther.lua
+++ linux/.config/nvim/lua/lazy/plugins/color-highligther.lua
@@ -1,4 +1,4 @@
-{
+return{
     "brenoprata10/nvim-highlight-colors",
     config = function()
         require("nvim-highlight-colors").setup({})
