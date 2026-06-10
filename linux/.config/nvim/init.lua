204e8ad26ea61567373799b7a79a2315cfa4ee85
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2026-06-10 14:59:37 -0700

    mappings

--- linux/.config/nvim/init.lua
+++ linux/.config/nvim/init.lua
@@ -2,7 +2,7 @@ require('options')
 vim.g.dotnet_errors_only = true
 vim.g.dotnet_show_project_file = false
 require('keymaps')
-require('mappings')
 require("lazy.bootstrap")
 require("lazy").setup(require("lazy.config"))
 -- si estas usando un compilador primero :compiler <your-compiler> and :make
+require('mappings')
--- linux/.config/nvim/lua/mappings.lua
+++ linux/.config/nvim/lua/mappings.lua
@@ -4,8 +4,8 @@ local opts = { noremap = true, silent = true }
 map('n', '<leader>qo', '<cmd>copen<CR>', vim.tbl_extend('force', opts, { desc = 'Open quickfix' }))
 map('n', '<leader>qc', '<cmd>cclose<CR>', vim.tbl_extend('force', opts, { desc = 'Close quickfix' }))
 map(
-    "n",
-    "<leader>ds",
+    'n',
+    '<leader>ds',
     "<cmd>lua require('telescope.builtin').diagnostics({ bufnr = 0 })<CR>",
-    opts, { desc = 'Buffer diagnostics' }
+    vim.tbl_extend('force', opts, { desc = 'Buffer diagnostics' })
 )
