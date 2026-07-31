11246858d39b935c04e520894625424800abb444
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2026-07-31 14:20:58 -0700

    opencode config

--- reload.sh
+++ reload.sh
@@ -1,7 +1,21 @@
+#!/usr/bin/env bash
+
 rm -rf ~/.config/nvim
 rm -rf ~/.config/opencode
 
 cp -r linux/.config/nvim ~/.config/nvim
 cp -r linux/.config/opencode ~/.config/opencode
 
-export OBSIDIAN_VAULT="/home/javi/notes/js-ts/"
+
+
+export GIT_REPO_NAME=""
+
+export GITHUB_PERSONAL_ACCESS_TOKEN=""
+
+export POSTGRES_USER=""
+export POSTGRES_PASSWORD=""
+export POSTGRES_DB=""
+
+export OBSIDIAN_VAULT=""
+
+export MONGODB_CONNECTION_STRING=""
