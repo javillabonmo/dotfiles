5f1efef06c5d24e0908bc675a47549c3d9f5b19a
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2026-09-17 10:05:15 -0500

    nothing important tbh

--- linux/amazon_linux/setup.sh
+++ linux/amazon_linux/setup.sh
@@ -5,17 +5,7 @@ sudo dnf install -y \
     vim \
     htop \
     python3 \
-    dnf-automatic
     
-#sudo vim /etc/dnf/automatic.conf
-
 curl -LsSf https://astral.sh/uv/install.sh | sh
-curl -fsSL https://bun.sh/install | bash
-
-# Agregar Bun al PATH en .bashrc
-echo 'export BUN_INSTALL="$HOME/.bun"' >> "$HOME/.bashrc"
-echo 'export PATH="$BUN_INSTALL/bin:$PATH"' >> "$HOME/.bashrc"
-source "$HOME/.bashrc"
-
 timedatectl set-timezone "America/Bogota"
 
--- linux/wsl/install.sh
+++ linux/wsl/install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-mkdir -p ~/.oh-my-zsh/completions
-docker completion zsh > ~/.oh-my-zsh/completions/_docker
-
 sudo apt install dotnet-sdk-10.0 -y
